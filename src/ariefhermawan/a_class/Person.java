7c7d154506a33f8efb3aadaffb46ed74ec613cb0
Arief-Karditya <nolifegansaya@gmail.com>
2024-10-06 11:25:35 +0700

    Class, Revisited

---
+++ src/ariefhermawan/a_class/Person.java
@@ -0,0 +1,8 @@
+package ariefhermawan.a_class;
+
+public class Person {
+    /**
+     * Ini adalah Class di Java.
+     * Bisa digunakan untuk membuat Object yang nanti disimpan pada variable.
+     * */
+}

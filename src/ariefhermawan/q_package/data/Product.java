4359353a88628aa8e900e32b1846cdf9ddcd9516
Arief-Karditya <nolifegansaya@gmail.com>
2024-10-08 16:10:30 +0700

    Package, Revisited

---
+++ src/ariefhermawan/q_package/data/Product.java
@@ -0,0 +1,16 @@
+/**
+ * <p>
+ *     Baris kode ke 6 adalah package.
+ * </p>
+ * */
+package ariefhermawan.q_package.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
